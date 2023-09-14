4d6dcb60f39c239bb6a2d5c41687175f4f329568
Sanghyeon Sim <5338095@gmail.com>
2023-09-14 16:17:03 +0900

    ✅ add test code

---
+++ test/domain/entities/todo_entity_test.dart
@@ -0,0 +1,14 @@
+// test/domain/entities/todo_entity_test.dart
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_todo_example/domain/entities/todo_entity.dart';
+
+void main() {
+  test('TodoEntity should be created with given values', () {
+    final todo = TodoEntity('1', 'Test Todo', false);
+
+    expect(todo.id, '1');
+    expect(todo.title, 'Test Todo');
+    expect(todo.isCompleted, false);
+  });
+}
